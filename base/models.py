96e796cad765ebce028c86c04abfeaf3f3d03d9f
Muhammad Ganim <hamudy250@gmail.com>
2026-03-29 14:29:43 +0300

    Admin system is ready

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name='العنوان')
+    content = models.TextField(verbose_name='المحتوى')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        return self.title
--- base/views.py
+++ base/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def home(request):
-    return render(request, 'base/home.html')
+    posts = Post.objects.all()
+    return render(request, 'base/home.html', {
+        'posts': posts,
+    })
